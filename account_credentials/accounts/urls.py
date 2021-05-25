871086e9bac6172d2aa73b7eaf682fce282ffb29
DemetriceW20 <demetrice_williams@hotmail.com>
2021-05-24 18:41:36 -0800

    Added redirects to homepage. Added form in register template. Added views to accounts app.

--- account_credentials/accounts/urls.py
+++ account_credentials/accounts/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 app_name = "accounts"
 urlpatterns = [
-    path('')
+    path('register/', views.register, name="register")
 ]
--- account_credentials/accounts/views.py
+++ account_credentials/accounts/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CustomUserForm
+from django.contrib.auth import login
 
-# Create your views here.
+
+def home(request):
+    return render(request, template_name='accounts/homepage.html')
+
+
+def register(request):
+    if request.method == "POST":
+        form = CustomUserForm(request.post)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            print("Registration Successful")
+            return redirect('accounts:home')
+    form = CustomUserForm
+    return render(request, template_name="accounts/register.html", context={"form": form})
+
+
