7846e62d4cc1ae01ca3c2757fce4df527973c745
DemetriceW20 <demetrice_williams@hotmail.com>
2021-05-24 18:06:17 -0800

    Created superuser. Created base template and register template.

--- account_credentials/accounts/urls.py
+++ account_credentials/accounts/urls.py
@@ -1,18 +1,7 @@
-from django import forms
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from django.urls import path
+from . import views
 
-
-class CustomUserForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-
-    class Meta:
-        model = User
-        fields = ("username", "email", "password1", "password2")
-
-    def save(self, commit=True):
-        user = super(CustomUserForm, self).save(commit=False)
-        user.email = self.cleaned_data["email"]
-        if commit:
-            user.save()
-        return user
+app_name = "accounts"
+urlpatterns = [
+    path('')
+]
