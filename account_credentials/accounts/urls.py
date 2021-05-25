ab15076c18ca19578c946b568d3c7094e96878e8
DemetriceW20 <demetrice_williams@hotmail.com>
2021-05-24 18:58:09 -0800

    Added new view for accounts to redirect to homepage.html

--- account_credentials/accounts/urls.py
+++ account_credentials/accounts/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name = "accounts"
 urlpatterns = [
-    path('register/', views.register, name="register")
+    path('', views.home, name='home'),
+    path('register/', views.RegisterView.as_view(), name='register')
 ]
--- account_credentials/accounts/views.py
+++ account_credentials/accounts/views.py
@@ -1,21 +1,28 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 from .forms import CustomUserForm
 from django.contrib.auth import login
+from django.urls import reverse_lazy
+from django.views import generic
+from .forms import CustomUserForm
 
 
 def home(request):
     return render(request, template_name='accounts/homepage.html')
 
 
-def register(request):
-    if request.method == "POST":
-        form = CustomUserForm(request.post)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            print("Registration Successful")
-            return redirect('accounts:home')
-    form = CustomUserForm
-    return render(request, template_name="accounts/register.html", context={"form": form})
+# def register(request):
+#     if request.method == "POST":
+#         form = CustomUserForm(request.post)
+#         if form.is_valid():
+#             user = form.save()
+#             login(request, user)
+#             print("Registration Successful")
+#             return redirect('accounts:home')
+#     form = CustomUserForm
+#     return render(request, template_name="accounts/register.html", context={"form": form})
 
+class RegisterView(generic.CreateView):
+    form_class = CustomUserForm
+    success_url = reverse_lazy('accounts:home')
+    template_name = 'accounts/register.html'
 
